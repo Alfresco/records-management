b320dde0941d6acb9bf55301a0d88d9ad4b86601
Tuna Aksoy <tuna.aksoy@alfresco.com>
2012-11-02 14:20:42 +0000

    RM-489: It was not possible to create an rm site with an empty data base
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@43340 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit 37aa8a9bea052cac9c698bdd52f40d9177618175

--- rm-share/config/alfresco/site-webscripts/org/alfresco/rm/components/dashlets/rm-utils.js
+++ rm-share/config/alfresco/site-webscripts/org/alfresco/rm/components/dashlets/rm-utils.js
@@ -3,6 +3,13 @@ function isRmAdmin(conn)
    var roles = conn.get("/api/rma/admin/rmroles?user=" + encodeURIComponent(user.id));
    var rolesData = eval('(' + roles + ')').data;
 
+   if (isEmpty(rolesData))
+   {
+      // No roles found. Probably there is no file plan
+      // Check if the user is "admin"
+      return user.isAdmin;
+   }
+
    for (var role in rolesData)
    {
       if (rolesData[role].name === "Administrator")
@@ -11,4 +18,13 @@ function isRmAdmin(conn)
       }
    }
    return false;
+}
+
+function isEmpty(jsonObject)
+{
+   var key;
+   for (key in jsonObject) {
+      return false;
+   }
+   return true;
 }
