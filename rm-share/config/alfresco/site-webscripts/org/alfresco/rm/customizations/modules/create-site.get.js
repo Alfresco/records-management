5079138da175f1cd432d07329c440cc89d6a2dec
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-01-21 10:27:17 +0000

    RM-576 (A user can use the create site dialog to create a records management site)  * Creating an RM site is not restricted to admin users anymore
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@45634 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit 6fc9c1cbb58a3041d7539c46b98b0ebe4f2aa1c7

--- rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/modules/create-site.get.js
+++ rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/modules/create-site.get.js
@@ -1,11 +1,5 @@
-<import resource="classpath:/alfresco/site-webscripts/org/alfresco/rm/components/dashlets/rm-utils.js">
-
-// Check if the user is an RM Admin
-if (isRmAdmin(remote.connect("alfresco")))
+model.sitePresets.push(
 {
-   model.sitePresets.push(
-   {
-      id: "rm-site-dashboard",
-      name: msg.get("type.recordsManagementSite")
-   });
-}
+   id: "rm-site-dashboard",
+   name: msg.get("type.recordsManagementSite")
+});
