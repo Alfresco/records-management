5c23fe62390d0ffb01f743125c8634cff46e8ba5
Roy Wetherall <roy.wetherall@alfresco.com>
2013-04-19 03:08:58 +0100

    RM: Revert HEAD to 4.2.b dependancy
     * r49372
    
    
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@49413 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit 1d3f016a1fe0d5b36d361896e4eb2db061330945

--- rm-share/config/alfresco/site-webscripts/org/alfresco/rm/components/documentlibrary/documentlist.get.head.ftl
+++ rm-share/config/alfresco/site-webscripts/org/alfresco/rm/components/documentlibrary/documentlist.get.head.ftl
@@ -4,7 +4,3 @@
 <@link rel="stylesheet" type="text/css" href="${url.context}/res/rm/components/documentlibrary/documentlist.css" />
 <@script type="text/javascript" src="${url.context}/res/components/documentlibrary/documentlist.js"></@script>
 <@script type="text/javascript" src="${url.context}/res/rm/components/documentlibrary/documentlist.js"></@script>
-<@script type="text/javascript" src="${url.context}/res/components/documentlibrary/documentlist-view-detailed.js"></@script>
-<@script type="text/javascript" src="${url.context}/res/components/documentlibrary/documentlist-view-simple.js"></@script>
-<@script type="text/javascript" src="${url.context}/res/components/documentlibrary/documentlist-view-gallery.js"></@script>
-<@script type="text/javascript" src="${url.context}/res/yui/slider/slider.js" group="documentlibrary"/>
--- rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/share/header/share-header.get.js
+++ rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/share/header/share-header.get.js
@@ -1,12 +1,26 @@
-<import resource="classpath:/alfresco/site-webscripts/org/alfresco/rm/customizations/share/share-utils.js">
+function getWidgetById(widgets, widgetId)
+{
+   var widget = null;
+   for (var i = 0, length = widgets.length; i < length; i++)
+   {
+      if (widgets[i].id === widgetId)
+      {
+         widget = widgets[i];
+         break;
+      }
+   }
+   if (!widget)
+   {
+      throw "Could not find a widget with the id '" + widgetId + "'.";
+   }
+   return widget;
+}
 
 // Add the "Management Console" Link to header navigation menu bar
 var shareVerticalLayoutWidget = getWidgetById(model.jsonModel.widgets, "SHARE_VERTICAL_LAYOUT"),
    headerTitleBarWidget = getWidgetById(shareVerticalLayoutWidget.config.widgets, "HEADER_TITLE_BAR"),
    headerNavigationMenuBarWidget = getWidgetById(headerTitleBarWidget.config.widgets, "HEADER_NAVIGATION_MENU_BAR"),
    siteNavigationWidgets = headerNavigationMenuBarWidget.config.widgets;
-
-//FIXME: Add "Management Console" Link
 
 // Add the customize dashboard link for RM pages
 if (page.titleId == "page.rmSiteDashboard.title")
