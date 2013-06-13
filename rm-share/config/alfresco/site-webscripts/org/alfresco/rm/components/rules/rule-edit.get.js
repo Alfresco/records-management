0eded3ac4cda20ca1e7fd121f720ffea58a9b479
Craig Tan <craig.tan@alfresco.com>
2013-06-13 05:29:09 +0100

    RM-695: Consolidation of RM actions appearing in RM rule UI
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@50977 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit 5b506a6c6c63e9af18c1a8e82e69d7137f79520d

--- rm-share/config/alfresco/site-webscripts/org/alfresco/rm/components/rules/rule-edit.get.js
+++ rm-share/config/alfresco/site-webscripts/org/alfresco/rm/components/rules/rule-edit.get.js
@@ -1,1 +1,16 @@
 <import resource="classpath:alfresco/site-webscripts/org/alfresco/components/rules/rule-edit.get.js">
+
+function rm_main()
+{
+	   var scripts;
+
+	   if (model.constraints != null) {
+	      scripts = model.constraints["rm-ac-scripts"];
+		  
+		  if (scripts != null) {
+	         return scripts;
+		  }
+	   }
+	   return model.scripts;
+}
+model.scripts = rm_main();
