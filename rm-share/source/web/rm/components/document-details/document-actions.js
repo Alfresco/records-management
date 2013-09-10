d379c73049fe0bdce9da365e8bc5e5e60a3806ef
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-09-10 12:18:47 +0100

    RM-893 (Failure when performing two actions on record's details page one by one without refreshing the page - script fails)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@55172 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit 931b3199619abcae83674b73585a8b5061bc3bb9

--- rm-share/source/web/rm/components/document-details/document-actions.js
+++ rm-share/source/web/rm/components/document-details/document-actions.js
@@ -16,10 +16,10 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 /**
  * Document actions component - RM extensions.
- * 
+ *
  * @namespace Alfresco
  * @class Alfresco.rm.component.DocumentActions
  */
@@ -27,7 +27,7 @@
 {
    /**
     * RecordsDocumentActions constructor.
-    * 
+    *
     * @param {String} htmlId The HTML id of the parent element
     * @return {Alfresco.rm.component.DocumentActions} The new RecordsDocumentActions instance
     * @constructor
@@ -35,15 +35,18 @@
    Alfresco.rm.component.DocumentActions = function(htmlId)
    {
       Alfresco.rm.component.DocumentActions.superclass.constructor.call(this, htmlId);
-      YAHOO.Bubbling.on("metadataRefresh", this.doRefresh, this);
+      YAHOO.Bubbling.on("metadataRefresh", function() {
+         YAHOO.Bubbling.unsubscribe("metadataRefresh", this.doRefresh, this);
+         window.location.reload(true);
+      }, this);
       return this;
    };
-   
+
    /**
     * Extend from Alfresco.DocumentActions
     */
    YAHOO.extend(Alfresco.rm.component.DocumentActions, Alfresco.DocumentActions);
-   
+
    /**
     * Augment prototype with RecordActions module, ensuring overwrite is enabled
     */
@@ -56,7 +59,6 @@
    {
       doRefresh: function RecordsDocumentActions_doRefresh()
       {
-         YAHOO.Bubbling.unsubscribe("metadataRefresh", this.doRefresh, this);
          var url = 'rm/components/document-details/document-actions?nodeRef={nodeRef}&container={containerId}';
          url += this.options.siteId ? '&site={siteId}' :  '';
          this.refresh(url);
