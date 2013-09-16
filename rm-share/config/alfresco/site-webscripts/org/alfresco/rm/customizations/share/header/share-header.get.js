1052b0496613c4af256e0d76a455537462fd3a1c
Tuna Aksoy <tuna.aksoy@alfresco.com>
2013-09-16 16:06:05 +0100

    RM-960 (Non admin users are not able to access the RM site)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@55346 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit f8f7e58bec1648d37146a41cc0dd5b6dfefc4958

--- rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/share/header/share-header.get.js
+++ rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/share/header/share-header.get.js
@@ -26,16 +26,37 @@ if (page.titleId == "page.rmSiteDashboard.title")
 {
    // FIXME: Id changes in share-header breaks RM backwards Compatibility to 4.2.d
    // Change this implementation after releasing 4.2.e
-   var titleBarModel = getTitleBarModel();
-   var widgets = titleBarModel[1].config.widgets;
-   widgets.unshift({
-      name: "alfresco/menus/AlfMenuItem",
-      config: {
-         id: "HEADER_CUSTOMIZE_SITE_DASHBOARD",
-         label: "customize_dashboard.label",
-         iconClass: "alf-cog-icon",
-         targetUrl: "site/" + page.url.templateArgs.site + "/customise-site-dashboard"
+   var titleBarModel = getTitleBarModel(),
+      customizeSiteDashboard = getCustomizeSiteDashboard(titleBarModel);
+   if (customizeSiteDashboard)
+   {
+      var widgets = customizeSiteDashboard.config.widgets;
+      widgets.unshift({
+         name: "alfresco/menus/AlfMenuItem",
+         config: {
+            id: "HEADER_CUSTOMIZE_SITE_DASHBOARD",
+            label: "customize_dashboard.label",
+            iconClass: "alf-cog-icon",
+            targetUrl: "site/" + page.url.templateArgs.site + "/customise-site-dashboard"
+         }
+      });
+      widgetUtils.findObject(model.jsonModel, "id", "HEADER_TITLE_MENU").config.widgets = titleBarModel;
+   }
+}
+
+function getCustomizeSiteDashboard(titleBarModel)
+{
+   var result,
+      i,
+      length = titleBarModel.length;
+
+   for (i = 0; i < length; i++)
+   {
+      if (titleBarModel[i].id == "HEADER_SITE_CONFIGURATION_DROPDOWN")
+      {
+         result = titleBarModel[i];
+         break;
       }
-   });
-   widgetUtils.findObject(model.jsonModel, "id", "HEADER_TITLE_MENU").config.widgets = titleBarModel;
+   }
+   return result;
 }
