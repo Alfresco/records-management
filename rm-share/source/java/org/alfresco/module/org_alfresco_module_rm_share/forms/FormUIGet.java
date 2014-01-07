a672e2d42c5245d2f9ea16379979848972470eb8
Roy Wetherall <roy.wetherall@alfresco.com>
2014-01-07 05:38:57 +0000

    RM-765: Remove DOD meta-data from core RM model
     * dod meta-data moved into record meta-data aspect within DOD model
     * by default dod compliance aspect is added to all records (via behaviour) .. will be configurable later (see RM-1116)
     * moved share related DOD config and spring context into separate files and package
     * fixed up RM form filters and overide of FormUIGet to allow the dynamic discovery and addition of form set's relating to the specially marked record meta-data aspects
     * clean up any references to the DOD model in the RM core code
     * Note that the patch for these changes is yet to be added
    
    
    
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@59445 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit 7258aa27e5511663533556bc4ef58443f8197652

--- rm-share/source/java/org/alfresco/module/org_alfresco_module_rm_share/forms/FormUIGet.java
+++ rm-share/source/java/org/alfresco/module/org_alfresco_module_rm_share/forms/FormUIGet.java
@@ -18,18 +18,28 @@
  */
 package org.alfresco.module.org_alfresco_module_rm_share.forms;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.alfresco.web.config.forms.FormSet;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
+ * Form UI GET method implementation override.
+ * <p>
+ * Allows custom and record metadata to be automatically displayed without need for form modificiations.
+ * 
  * @author Roy Wetherall
  */
 public class FormUIGet extends org.alfresco.web.scripts.forms.FormUIGet
 {
     private static final String SET_RM_CUSTOM = "rm-custom";
     private static final String SET_RM_METADATA = "rm-metadata";
+    private static final String LABEL_ID_PREFIX = "label.set.";
     
     /**
      * @see org.alfresco.web.scripts.forms.FormUIGet#getVisibleFieldsInSet(org.alfresco.web.scripts.forms.FormUIGet.ModelContext, org.alfresco.web.config.forms.FormSet)
@@ -54,17 +64,110 @@ public class FormUIGet extends org.alfresco.web.scripts.forms.FormUIGet
         return result;
     }
     
-    // TODO .. we will need to override this so that we can add new set's automatically!
-    //      .. atm we have to hard code the set into the form definition
+    /** 
+     * @see org.alfresco.web.scripts.forms.FormUIGet#processVisibleFields(org.alfresco.web.scripts.forms.FormUIGet.ModelContext)
+     */
+    @Override
+    protected void processVisibleFields(ModelContext context)
+    {
+        // iterate over the root sets and generate a model for each one
+        for (FormSet setConfig : getRootSetsAsList(context))
+        {
+            Set set = generateSetModelUsingVisibleFields(context, setConfig);
+            
+            // if the set got created (as it contained fields or other sets) 
+            // add it to the structure list in the model context
+            if (set != null)
+            {
+                context.getStructure().add(set);
+            }
+        }
+    }
     
-//    @Override
-//    protected void processServerFields(ModelContext context)
-//    {
-//        super.processServerFields(context);
-//        
-//        // try adding your custom set's here
-//        Set mySet = new Set("mysetid", "My Set");
-//        context.getStructure().addSet(mySet);
-//    }
-
+    /**
+     * @see org.alfresco.web.scripts.forms.FormUIGet#processServerFields(org.alfresco.web.scripts.forms.FormUIGet.ModelContext)
+     */
+    @Override
+    protected void processServerFields(ModelContext context)
+    {
+        if (context.getFormConfig() != null)
+        {
+            // discover the set membership of the fields using the form definition
+            Map<String, List<String>> setMembership = discoverSetMembership(context);
+            
+            // get root sets from config and build set structure using config and lists built above
+            for (FormSet setConfig : getRootSetsAsList(context))
+            {
+                Set set = generateSetModelUsingServerFields(context, setConfig, setMembership);
+                
+                // if the set got created (as it contained fields or other sets) 
+                // add it to the structure list in the model context
+                if (set != null)
+                {
+                    context.getStructure().add(set);
+                }
+            }
+        }
+        else
+        {
+            // as there is no config at all generate a default set that contains
+            // all the fields returned in the form definition
+            Set set = generateDefaultSetModelUsingServerFields(context);
+            context.getStructure().add(set);
+        }
+    }
+    
+    /**
+     * Gets the root sets as a list, including the dynamically discovered record meta-data sets.
+     * 
+     * @param context
+     * @return
+     */
+    protected List<FormSet> getRootSetsAsList(ModelContext context)
+    {
+        List<FormSet> result = context.getFormConfig().getRootSetsAsList();
+        result.addAll(getRecordMetaDataSetConfig(context));
+        return result;
+    }
+    
+    /**
+     * Gets all the record meta-data sets present in the form data.
+     * 
+     * @param context
+     * @return
+     */
+    protected Collection<FormSet> getRecordMetaDataSetConfig(ModelContext context)
+    {
+        Map<String, FormSet> result = new HashMap<String, FormSet>(13);
+        
+        try
+        {
+            // get list of fields from form definition
+            JSONObject data = context.getFormDefinition().getJSONObject(MODEL_DATA);
+            
+            JSONObject definition = data.getJSONObject(MODEL_DEFINITION);            
+            JSONArray fieldsFromServer = definition.getJSONArray(MODEL_FIELDS);
+            
+            // iterate around fields and pull out the record metadata sets
+            for (int x = 0; x < fieldsFromServer.length(); x++)
+            {
+                JSONObject fieldDefinition = fieldsFromServer.getJSONObject(x); 
+                if (fieldDefinition.has(MODEL_GROUP))
+                {
+                    String set = fieldDefinition.getString(MODEL_GROUP);
+                    if (result.containsKey(set) == false && set.startsWith(SET_RM_METADATA) == true)
+                    {                        
+                        FormSet formSet = new FormSet(set, null, "panel", null, LABEL_ID_PREFIX + set);
+                        result.put(set, formSet);
+                    }
+                }
+            }
+        }
+        catch (JSONException je)
+        {
+            // do nothing 
+        }
+        
+        return result.values();        
+    }
 }
