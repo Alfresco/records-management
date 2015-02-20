570187d5839607c289dbf68e3d682436f2a6b98c
Tuna Aksoy <tuna.aksoy@alfresco.com>
2015-02-20 14:37:50 +0000

    RM-1949 (Details page is displayed incorrect after link to rule set)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/BRANCHES/V2.3@97603 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit 0e09cc470c0b111f116f5e591e3dd7b297ccbdfc

---
+++ rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/components/rules/rules-linked.get.html.ftl
@@ -0,0 +1,3 @@
+<@markup id="rm-js" target="js" action="after" scope="global">
+   <@script src="${url.context}/res/rm/components/rules/rules-linked.js" group="rules"/>
+</@markup>
---
+++ rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/components/rules/rules-linked.get.js
@@ -0,0 +1,8 @@
+<import resource="classpath:alfresco/site-webscripts/org/alfresco/components/rules/rules-linked.get.js">
+
+function rm_main()
+{
+   model.widgets[0].name = "Alfresco.rm.module.RulesLinked";
+}
+
+rm_main();
