a39a1adc99ecfd4250b5facd82013a1558e585e8
David Webster <david.webster@alfresco.com>
2015-03-31 14:23:12 +0100

    RM-2078: Classify Document Action, also includes work for RM-2052 +review RM @taksoy
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@100800 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit d9f476a1545c3e1fcca02967d4cc8df1d422fd85

---
+++ rm-share/source/java/org/alfresco/module/org_alfresco_module_rm_share/evaluator/canUserClassify.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.module.org_alfresco_module_rm_share.evaluator;
+
+import org.alfresco.error.AlfrescoRuntimeException;
+import org.alfresco.util.ParameterCheck;
+import org.alfresco.web.evaluator.BaseEvaluator;
+import org.json.simple.JSONObject;
+
+/**
+ * Checks if the user is a record contributor
+ *
+ * @author David Webster
+ * @since 3.0
+ * @todo Currently stubbed out. Needs completing.
+ */
+public class canUserClassify extends BaseEvaluator {
+    /**
+     * json names
+     */
+    private static final String NO_CLEARANCE = "no clearance";
+
+    /**
+     * @see org.alfresco.web.evaluator.BaseEvaluator#evaluate(org.json.simple.JSONObject)
+     */
+    @Override
+    public boolean evaluate(JSONObject jsonObject) {
+
+        boolean result = false;
+
+        try {
+            // TODO: This should retrieve the user's security clearance level and make sure that it is set and doesn't equal NO_CLEARANCE
+            result = true;
+        } catch (Exception err) {
+            throw new AlfrescoRuntimeException("Exception whilst running UI evaluator: " + err);
+        }
+
+        return result;
+    }
+}
