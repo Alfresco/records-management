7494d3053ee7545d30489f1561d9a37220c5c477
Tuna Aksoy <tuna.aksoy@alfresco.com>
2015-04-08 10:18:58 +0100

    RM-1827 ("Declare" actions become available for user only after re-login)  * Changes reverted
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@101292 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit 8f97f575e5cfa5453dcbabcae1939938d2d08000

--- rm-share/source/java/org/alfresco/module/org_alfresco_module_rm_share/evaluator/IsUserRecordContributor.java
+++ rm-share/source/java/org/alfresco/module/org_alfresco_module_rm_share/evaluator/IsUserRecordContributor.java
@@ -18,10 +18,15 @@
  */
 package org.alfresco.module.org_alfresco_module_rm_share.evaluator;
 
+import java.util.Collections;
+
 import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.util.ParameterCheck;
 import org.alfresco.web.evaluator.BaseEvaluator;
+import org.alfresco.web.extensibility.SlingshotEvaluatorUtil;
 import org.json.simple.JSONObject;
+import org.springframework.extensions.surf.RequestContext;
+import org.springframework.extensions.surf.support.ThreadLocalRequestContext;
 
 /**
  * Checks if the user is a record contributor
@@ -33,7 +38,18 @@ public class IsUserRecordContributor extends BaseEvaluator
 {
     /** json names */
     private static final String IS_RECORD_CONTRIBUTOR_GROUP_ENABLED = "isRecordContributorGroupEnabled";
-    private static final String RM_SHOW_ACTIONS = "rmShowActions";
+    private static final String RECORD_CONTRIBUTOR_GROUP_NAME = "recordContributorGroupName";
+
+    /** slingshot evaluator util */
+    protected SlingshotEvaluatorUtil util = null;
+
+    /**
+     * @param slingshotExtensibilityUtil    {@link SlingshotEvaluatorUtil}
+     */
+    public void setSlingshotEvaluatorUtil(SlingshotEvaluatorUtil slingshotExtensibilityUtil)
+    {
+        util = slingshotExtensibilityUtil;
+    }
 
     /**
      * @see org.alfresco.web.evaluator.BaseEvaluator#evaluate(org.json.simple.JSONObject)
@@ -41,29 +57,34 @@ public class IsUserRecordContributor extends BaseEvaluator
     @Override
     public boolean evaluate(JSONObject jsonObject)
     {
-        ParameterCheck.mandatory("jsonObject", jsonObject);
-
         boolean result = true;
+        ParameterCheck.mandatory("jsonObject", jsonObject);
         try
         {
             JSONObject node = (JSONObject) jsonObject.get("node");
             if (node != null)
             {
-                if ((Boolean) node.get(IS_RECORD_CONTRIBUTOR_GROUP_ENABLED))
+                boolean isEnabled = (Boolean)node.get(IS_RECORD_CONTRIBUTOR_GROUP_ENABLED);
+                if (isEnabled)
                 {
-                    result = (Boolean) node.get(RM_SHOW_ACTIONS);
+                    // get the name of the record contributor group
+                    String groupName = (String)node.get(RECORD_CONTRIBUTOR_GROUP_NAME);
+
+                    // check the record contributor group
+                    final RequestContext rc = ThreadLocalRequestContext.getRequestContext();
+                    result = util.isMemberOfGroups(rc, Collections.singletonList("GROUP_" + groupName), true);
                 }
-            }
-            else
-            {
-                result = false;
+                else
+                {
+                    // if group check is not enabled then allow
+                    result = true;
+                }
             }
         }
         catch (Exception err)
         {
             throw new AlfrescoRuntimeException("Exception whilst running UI evaluator: " + err.getMessage());
-        }
-
+        };
         return result;
     }
 }
