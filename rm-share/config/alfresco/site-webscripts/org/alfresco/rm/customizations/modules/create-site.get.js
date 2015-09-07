d9289e5344e208bef115ed69f805177c4e6015ac
Tuna Aksoy <tuna.aksoy@alfresco.com>
2015-09-07 12:47:28 +0100

    RM-2575 (Investigate compatibility issues with 5.1)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-enterprise/modules/recordsmanagement/HEAD@111555 c4b6b30b-aa2e-2d43-bbcb-ca4b014f7261
    
    Mirrored from commit 2fbbdee38ed0483d72e2332df97f605b8fd915b6

--- rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/modules/create-site.get.js
+++ rm-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/modules/create-site.get.js
@@ -6,15 +6,18 @@ model.sitePresets.push(
 });
 
 // compliance options
-var compliance = 
+var compliance =
 [
    {
-      id: "{http://www.alfresco.org/model/recordsmanagement/1.0}rmsite", 
+      id: "{http://www.alfresco.org/model/recordsmanagement/1.0}rmsite",
       name: msg.get("compliance.standard")
-  }, 
+  },
   {
-      id: "{http://www.alfresco.org/model/dod5015/1.0}site", 
+      id: "{http://www.alfresco.org/model/dod5015/1.0}site",
       name: msg.get("compliance.dod5015")
   }
 ];
 model.compliance = compliance;
+
+// FIXME: This should be removed after updating the current Alfresco dependency (5.1.a-EA)
+model.sitePresetsClass = (model.sitePresets.length == 1) ? " hidden" : "";
