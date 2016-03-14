b7ced05a49717c1073b26f5936c571c64ef8fc1d
Tuna Aksoy <tuna.aksoy@alfresco.com>
2016-03-14 16:05:44 +0000

    RM-3132 (Update license headers)
    Mirrored from commit 8372c32a389883e9f005aa9c6ca83b31e65ece11

--- rm-community/rm-community-share/config/alfresco/site-webscripts/org/alfresco/rm/components/upload/dnd-upload.get.js
+++ rm-community/rm-community-share/config/alfresco/site-webscripts/org/alfresco/rm/components/upload/dnd-upload.get.js
@@ -1,15 +1,13 @@
-<import resource="classpath:alfresco/site-webscripts/org/alfresco/components/upload/dnd-upload.get.js">
-
 /*
  * #%L
  * Alfresco Records Management Module
  * %%
  * Copyright (C) 2005 - 2016 Alfresco Software Limited
  * %%
- * This file is part of the Alfresco software. 
+ * This file is part of the Alfresco software.
  * 
- * If the software was purchased under a paid Alfresco license, the terms of 
- * the paid license agreement will prevail.  Otherwise, the software is 
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
  * provided under the following open source license terms:
  * 
  * Alfresco is free software: you can redistribute it and/or modify
@@ -26,5 +24,6 @@
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
+<import resource="classpath:alfresco/site-webscripts/org/alfresco/components/upload/dnd-upload.get.js">
 
 model.widgets[0].name = "Alfresco.rm.component.DNDUpload";
