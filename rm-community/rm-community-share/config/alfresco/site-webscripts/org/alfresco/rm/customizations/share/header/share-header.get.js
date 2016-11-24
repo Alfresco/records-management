0975de118c68191ed5a314c00ea89a193910ed0d
David Webster <david@cognite.net>
2016-11-24 23:39:35 +0000

    RM-4267: code formatting tweaks
    Mirrored from commit e43d2e71e3f64113367a718c62f3adb14b0effca

--- rm-community/rm-community-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/share/header/share-header.get.js
+++ rm-community/rm-community-share/config/alfresco/site-webscripts/org/alfresco/rm/customizations/share/header/share-header.get.js
@@ -1,4 +1,4 @@
-<import resource= "classpath:/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js" >
+//<import resource="classpath:/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js" >
 
 /*
  * #%L
