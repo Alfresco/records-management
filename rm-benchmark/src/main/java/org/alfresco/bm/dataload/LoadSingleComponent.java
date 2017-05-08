d9668290a2421f60f1718c80169784e578e2d6a6
Silviu Dinuta <silviu.dinuta@savatech.ro>
2017-05-08 16:58:15 +0100

    RM-4781: created test config and added LoadSingleComponent class
    Mirrored from commit d409dedf655c61084650fe1b8655e5c6b6ec075e

---
+++ rm-benchmark/src/main/java/org/alfresco/bm/dataload/LoadSingleComponent.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2005-2017 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.alfresco.bm.dataload;
+
+import org.alfresco.bm.event.Event;
+import org.alfresco.bm.event.EventResult;
+
+/**
+ * Common event for loading one single rm component. This event can load: one root record category, one sub-category, one record folder, one record, one root unfiled record folder,
+ * one child unfiled record folder, one unfiled record. The event can also file one unfiled record.
+ *
+ * @author Silviu Dinuta
+ * @since 2.6
+ *
+ */
+public class LoadSingleComponent extends RMBaseEventProcessor
+{
+    private String eventNameCompleted;
+    public void setEventNameCompleted(String eventNameCompleted)
+    {
+        this.eventNameCompleted = eventNameCompleted;
+    }
+
+    @Override
+    protected EventResult processEvent(Event event) throws Exception
+    {
+        return null;
+    }
+}
