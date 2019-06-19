c39e25feff55e78fd580ea5ab87711e790328d3e
Ross Gale <ross.gale@alfresco.com>
2019-06-19 14:38:19 +0100

    RM-6865 adding code to remove slash from empty path
    Mirrored from commit 0ee9f2808691c9bd93bfd73c518ff625191e78f8

--- rm-community/rm-community-share/source/web/components/documentlibrary/dm-actions.js
+++ rm-community/rm-community-share/source/web/components/documentlibrary/dm-actions.js
@@ -199,7 +199,7 @@
 
             var me = this;
             this.modules.copyMoveLinkFileTo.onOK = function DLTB_onOK(e, p_obj) {
-               assets.path = me.modules.copyMoveLinkFileTo.selectedNode.data.path;
+               assets.path = me.modules.copyMoveLinkFileTo.selectedNode.data.path.substr(1);
                me.onActionSimpleRepoAction(assets, owner);
                this.widgets.dialog.hide();
             }
